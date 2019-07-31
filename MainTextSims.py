c5a6a177884b0bbbe181dd037e493c1a9713c18e
asli munzur <admunzur.21@gmail.com>
2019-07-30 17:46:58 -0700

    commit at the end of the day, july 30

--- MainTextSims.py
+++ MainTextSims.py
@@ -114,7 +114,7 @@ def recomb(surv):
 	off_chrom1_index = off_chrom1 + even_nums.reshape(N_adapts[0], 1)
 	off_chrom2_index = off_chrom2 + even_nums.reshape(N_adapts[0], 1)
 
-	off = np.hstack((parent1[(off_chrom1_index)], parent2[(off_chrom2_index)])).reshape(N_adapts[0], (np.size(off1_chroms, 1))) #stack the same rows from two arrays together and reformat. each row is one offspring. 
+	off = np.hstack((parent1[(off_chrom1_index)], parent2[(off_chrom2_index)])).reshape(N_adapts[0], np.shape(parent1_gamete1)[1] * 2)       #(np.size(off1_chroms, 1))) #stack the same rows from two arrays together and reformat. each row is one offspring. 
 	
 	return off
 
@@ -151,21 +151,18 @@ def mutate(off, u, alpha, n, mut):
 	pop_chrom2 = np.append(pop_chrom2, zero, axis = 1) #append zero array to chrom2
 
 	#append pop_chrom1 and pop_chrom2 horizontally to make the pop matrix. each row is one individual. each row has the both chromosomes. left: chrom1 right: chrom2
-	pop = np.append(pop_chrom1, pop_chrom2, axis = 1)
+	pop_genotype = np.append(pop_chrom1, pop_chrom2, axis = 1) #both chromosomes present
+
+	pop_overall = (pop_chrom1 + pop_chrom2) / 2 #chromosomes averaged 
 
 	mut = np.vstack((mut, newmuts)) #append effect of new mutations to mutation list
 
-	#create the dominance coefficient array (h)
-	#pick random numbers (float) between 0 and 1. pick as many as the number of rows. of mut. only 1 column.
-	h = np.random.uniform(low = 0, high = 1, size = np.array(mut).shape[0]).reshape(np.array(mut).shape[0], 1)
-
-	return [pop, mut]
+	return [pop_genotype, pop_overall, mut]
 
 def which_index(pop):
 	return np.array([
 		i for i in range(len(pop))
-		if pop[i] == True
-	])
+		if pop[i] == False ])
 
 def remove_muts(pop, mut): #here pop is the same thing as off
 	"""
@@ -178,26 +175,34 @@ def remove_muts(pop, mut): #here pop is the same thing as off
 	pop_chrom1 = np.split(pop, 2, axis = 1)[0]
 	pop_chrom2 = np.split(pop, 2, axis = 1)[1]
 
-	keep_pop_chrom1 = np.array(pop_chrom1.any(axis=0)) #go through the columns of pop_chrom1 and determine if any column is all 0
-	keep_pop_chrom2 = np.array(pop_chrom2.any(axis=0))
+	# keep_pop_chrom1 = np.array(pop_chrom1.any(axis=0)) #go through the columns of pop_chrom1 and determine if any column is all 0
+	# keep_pop_chrom2 = np.array(pop_chrom2.any(axis=0))
 
-	keep = np.intersect1d(which_index(keep_pop_chrom1), which_index(keep_pop_chrom2)) #find the indices where the two arrays have 0 in the same columns 
+	chrom1_zero = np.where(~pop_chrom1.any(axis = 0))[0] #find the columns that are all zeros
+	chrom2_zero = np.where(~pop_chrom2.any(axis = 0))[0]
 
-	if len(keep) == 0:
+	remove = np.intersect1d(chrom1_zero, chrom2_zero) #find the indices where the two arrays have 0 in the same columns 
+
+	# keep = np.intersect1d(which_index(keep_pop_chrom1), which_index(keep_pop_chrom2)) 
+
+	if len(remove) == 0:
 		pop = np.hstack((pop_chrom1, pop_chrom2))
-		mut = mut 
 
 	else: 
-		pop_chrom1 = pop_chrom1[:, keep] #update the chroms by removing the columns with lost mutations
-		pop_chrom2 = pop_chrom2[:, keep]
+		pop_chrom1 = np.delete(pop_chrom1, remove, 1) # delete the columns that are all zeros 
+		pop_chrom2 = np.delete(pop_chrom1, remove, 1)
 		pop = np.hstack((pop_chrom1, pop_chrom2)) #horizontally reattach the chromosomes and make the pop
 
-		mut = mut[keep].tolist() #remove the lost loci by removing the rows
+		mut = np.delete(mut, remove, 0) #remove the lost loci by removing the related rows
 
 		#pop = mutate_result[0] it might be good to include these somewhere in the simulations if sth goes wrong 
 		#mut = mutate_result[1]
 	
 	return[pop, mut]
+
+
+		#pop = mutate_result[0] it might be good to include these somewhere in the simulations if sth goes wrong 
+		#mut = mutate_result[1]
 
 	#ORIGINAL CODE 
 	# if remove_lost:
@@ -225,7 +230,7 @@ data_dir = 'data'
 ##PARAMETERS FOR ADAPTING POPULATIONS##
 ######################################################################
 
-N_adapts = [200] #number of diploid individuals (positive integer)
+N_adapts = [10] #number of diploid individuals (positive integer)
 alpha_adapt = 0.1 #mutational sd (positive real number)
 u_adapt = 0.1 #mutation probability per generation per genome (0<u<1). if this is 0.5, this means half of the population is likely to mutate 
 sigma_adapts = [1] #selection strengths
@@ -298,10 +303,10 @@ def main():
 						popfound2 = np.array([[1]] * N_adapt) 
 						popfound = np.column_stack((popfound1, popfound2)) # create a diploid genotype. #of columns= #of chromosomes 
 
-						mutfound = mut = np.array([[0] * n]) #similar to above. filled with zeroes. number of coloumns: n. rows: 1. convert to a list 
+						mut = np.array([[0] * n]) #similar to above. filled with zeroes. number of coloumns: n. rows: 1. convert to a list 
 
-						[pop1, mut1] = [popfound, mutfound] # this creates pop and mut arrays for both parents. they are the same because we start from the same point. 
-						[pop2, mut2] = [popfound, mutfound] # mut1 = how farther you go from the origin due to mutations in pop1. same for mut2
+						[pop1, mut1] = [popfound, mut] # this creates pop and mut arrays for both parents. they are the same because we start from the same point. 
+						[pop2, mut2] = [popfound, mut] # mut1 = how farther you go from the origin due to mutations in pop1. same for mut2
 
 						# mut1_1 = np.split(mut1, 2, axis = 0)[0]
 						# mut1_2 = np.split(mut1, 2, axis = 0)[0]
@@ -315,6 +320,9 @@ def main():
 						pop2_chrom1 = popfound2 # 1st chromosome of pop2
 						pop2_chrom2 = popfound2 # 2nd chromosome of pop2
 
+						pop1_overall = ((pop1_chrom1 + pop1_chrom2) / 2 ) # two chromosomes of pop1 averaged
+						pop2_overall = ((pop2_chrom1 + pop2_chrom2) / 2 ) # two chromosomes of pop2 averaged
+						
 						# intitialize generation counter
 						gen = 0
 
@@ -326,20 +334,6 @@ def main():
 							# phenos2 = np.dot(pop2, mut2) #sum mutations held by each individual 
 
 							# genotype to phenotype (diploid):
-							pop1_overall = ((pop1_chrom1 + pop1_chrom2) / 2 ) # two chromosomes of pop1 averaged
-							pop2_overall = ((pop2_chrom1 + pop2_chrom2) / 2 ) # two chromosomes of pop2 averaged
-
-							#create the dominance coefficient array (h)
-							#pick random numbers (float) between 0 and 1. pick as many as the number of rows. of mut. only 1 column.
-							#replace the values 0.5 with the h. 
-							h_pop1 = np.random.uniform(low = 0, high = 1, size = len(pop1_overall[pop1_overall == 0.5]))
-							pop1_overall[pop1_overall == 0.5] = h_pop1
-
-							h_pop2 = np.random.uniform(low = 0, high = 1, size = len(pop2_overall[pop2_overall == 0.5]))
-							pop2_overall[pop2_overall == 0.5] = h_pop2
-
-							# mut = np.zeros(pop1_overall.shape[1]).reshape(n, int(pop1_overall.shape[1] / n))
-
 							phenos1 = np.dot(pop1_overall, mut1) #sum mutations held by each individual
 							phenos2 = np.dot(pop2_overall, mut2) #sum mutations held by each individual
 
@@ -348,16 +342,16 @@ def main():
 							w2 = fitness(phenos2, theta2, sigma_adapt)
 
 							# wright-fisher (multinomial) sampling
-							parents1 = np.random.multinomial(N_adapt, w1/sum(w1)) # number of times each parent chosen, drawing samples from a multinomial ditribution
+							# number of times each parent chosen, drawing samples from a multinomial ditribution
 							# N_adapt = number of experiments, w1/sum(w1 = probability of parent1 being chosen. if you are more fit, you are chosen more often. 
-							
-							off1_chrom1 = np.repeat(pop1_overall, parents1, axis=0) 
-							off1_chrom2 = np.repeat(pop1_overall, parents1, axis=0)
+							parents1 = np.random.multinomial(N_adapt, w1/sum(w1))
+							off1_chrom1 = np.repeat(pop1_overall, parents1, axis = 0) 
+							off1_chrom2 = np.repeat(pop1_overall, parents1, axis = 0)
 							off1 = [off1_chrom1, off1_chrom2]
 							
 							parents2 = np.random.multinomial(N_adapt, w2/sum(w2)) # number of times each parent chosen
-							off2_chrom1 = np.repeat(pop2_overall, parents2, axis=0) # offspring genotypes of pop2
-							off2_chrom2 = np.repeat(pop2_overall, parents2, axis=0)
+							off2_chrom1 = np.repeat(pop2_overall, parents2, axis = 0) # offspring genotypes of pop2
+							off2_chrom2 = np.repeat(pop2_overall, parents2, axis = 0)
 							off2 = [off2_chrom1, off2_chrom2]
 
 							# mating and recombination
@@ -365,12 +359,31 @@ def main():
 							off2 = recomb(off2)
 
 							# mutation and population update
-							[pop1_overall, mut1] = mutate(off1, u_adapt, alpha_adapt, n, mut1) #mutate_result[0] = pop,  mutate_result[1] = mut 
-							[pop2_overall, mut2] = mutate(off2, u_adapt, alpha_adapt, n, mut2)
+
+							# pop1_genotype = off1
+							# pop2_genotype = off2
+
+							[pop1_genotype, pop1_overall, mut1] = mutate(off1, u_adapt, alpha_adapt, n, mut1)
+							[pop2_genotype, pop2_overall, mut2] = mutate(off2, u_adapt, alpha_adapt, n, mut2)
+
+							#create the dominance coefficient array (h)
+							#pick random numbers (float) between 0 and 1. pick as many as the number of rows. of mut. only 1 column.
+							#replace the values 0.5 with the h. 
+							# h_pop1 = np.random.uniform(low = 0, high = 1, size = len(pop1_overall[pop1_overall == 0.5]))
+							# pop1_overall[pop1_overall == 0.5] = h_pop1
+
+							# h_pop2 = np.random.uniform(low = 0, high = 1, size = len(pop2_overall[pop2_overall == 0.5]))
+							# pop2_overall[pop2_overall == 0.5] = h_pop2
 
 							# remove lost mutations (all zero columns in pop)
-							[pop1_overall, mut1] = remove_muts(pop1_overall, mut1)
-							[pop2_overall, mut2] = remove_muts(pop2_overall, mut2)
+							[pop1_genotype, mut1] = remove_muts(pop1_genotype, mut1)
+							[pop2_genotype, mut2] = remove_muts(pop2_genotype, mut2)
+
+							pop1_genotype = [np.split(pop1_genotype, 2, axis = 1)[0], np.split(pop1_genotype, 2, axis = 1)[1]]
+							pop2_genotype = [np.split(pop2_genotype, 2, axis = 1)[0], np.split(pop2_genotype, 2, axis = 1)[1]]
+
+							off1 = pop1_genotype
+							off2 = pop2_genotype
 
 							# go to next generation
 							gen += 1
@@ -391,10 +404,16 @@ def main():
 						# offphenos = dict()
 						# offpheno = []
 
-						#HYBRIDIZATION
+						#HYBRIDIZATION - always choose parents from different populations 
 
 						#genotype of hybrids
-				
+
+						pop1_chrom1 = np.split(pop1_genotype, 2, axis = 1)[0]
+						pop1_chrom2 = np.split(pop1_genotype, 2, axis = 1)[1]
+
+						pop2_chrom1 = np.split(pop2_genotype, 2, axis = 1)[0]
+						pop2_chrom2 = np.split(pop2_genotype, 2, axis = 1)[1]
+						
 						#make the zero matrices
 						pop1_zero1 = np.zeros(len(pop1_chrom1) * pop2_chrom1.shape[1]).reshape(len(pop2_chrom1), pop2_chrom1.shape[1])
 						pop1_zero2 = np.zeros(len(pop1_chrom2) * pop2_chrom2.shape[1]).reshape(len(pop2_chrom2), pop2_chrom2.shape[1])
@@ -456,9 +475,13 @@ def main():
 						hybrid_overall_recomb1 = (F1_after_recomb1_chrom2 + F1_after_recomb1_chrom1) / 2
 						hybrid_overall_recomb2 = (F1_after_recomb2_chrom2 + F1_after_recomb2_chrom1) / 2
 
-						hybrid_overall_all = np.vstack((hybrid_overall_recomb1, hybrid_overall_recomb2))
+						hybrid_overall_all = np.vstack((hybrid_overall_recomb1, hybrid_overall_recomb2)) #stack all hybrids phenos. each row is one individual. 
+						
+						# h_hybrid = np.random.uniform(low = 0, high = 1, size = len(hybrid_overall_all[hybrid_overall_all == 0.5]))
+						# hybrid_overall_all[hybrid_overall_all == 0.5] = h_hybrid
 
 						hybrid_pheno = np.dot(hybrid_overall_all, mut_hybrid)
+
 
 
 						# #make each of nHybrids hybrids
@@ -503,7 +526,7 @@ def main():
 						# Efitmeanpheno = np.mean(fitness(np.array([Emeanpheno]), theta1, sigma_adapt))/pfit
 
 						# #save hybrid phenotype data (to make hybrid clouds)
-						pheno_data = np.column_stack( [np.array([i+1 for i in range(len(hybrid_pheno))]), np.array([rep+1]*len(hybrid_pheno)), np.array([round(angles[j]*180/math.pi,2)]*len(hybrid_pheno)), np.array([opt_dist * (2*(1-math.cos(angles[j])))**(0.5)]*len(hybrid_pheno)), hybrid_pheno]) #make hybrid phenotype data (with a bunch of identifiers in front of each phenotype)
+						pheno_data = np.column_stack([np.array([i+1 for i in range(len(hybrid_pheno))]), np.array([rep+1]*len(hybrid_pheno)), np.array([round(angles[j]*180/math.pi,2)]*len(hybrid_pheno)), np.array([opt_dist * (2*(1-math.cos(angles[j])))**(0.5)]*len(hybrid_pheno)), hybrid_pheno]) #make hybrid phenotype data (with a bunch of identifiers in front of each phenotype)
 						np.savetxt(fileHandle_B, pheno_data, fmt='%.3f', delimiter=',') #save
 
 						#save summary data
